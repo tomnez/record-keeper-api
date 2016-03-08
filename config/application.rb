91d9c3cc155e040c7d15ab884cdfc7a345152b93
Tom Netzband <tomnez@gmail.com>
2016-03-07 20:13:26 -0500

    add/remove gems

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RecordKeeperApi
   class Application < Rails::Application
+    config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
