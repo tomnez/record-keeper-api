13f943b48a36b8c5e0983878aeee5b924a93f34d
Tom Netzband <tomnez@gmail.com>
2016-03-08 18:29:37 -0500

    install and configure devise

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,13 @@ Bundler.require(*Rails.groups)
 module RecordKeeperApi
   class Application < Rails::Application
     config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
